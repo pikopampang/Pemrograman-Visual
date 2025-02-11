66e77855ee2703a4a1035934e98d5ec7db30b973
pikopampang <110894300+pikopampang@users.noreply.github.com>
2025-02-12 01:42:06 +0700

    Add files via upload

---
+++ UAS_PEMROGRAMAN_VISUAL/UAS_PEMROGRAMAN_VISUAL/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ContextMenuStrip2 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.txtAngka1 = New System.Windows.Forms.TextBox()
+        Me.cmbOperasi = New System.Windows.Forms.ComboBox()
+        Me.txtAngka2 = New System.Windows.Forms.TextBox()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.btnBacaHistory = New System.Windows.Forms.Button()
+        Me.lblHasil = New System.Windows.Forms.Label()
+        Me.txtHistory = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip3 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'ContextMenuStrip2
+        '
+        Me.ContextMenuStrip2.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ContextMenuStrip2.Name = "ContextMenuStrip2"
+        Me.ContextMenuStrip2.Size = New System.Drawing.Size(61, 4)
+        '
+        'txtAngka1
+        '
+        Me.txtAngka1.Location = New System.Drawing.Point(141, 51)
+        Me.txtAngka1.Name = "txtAngka1"
+        Me.txtAngka1.Size = New System.Drawing.Size(179, 26)
+        Me.txtAngka1.TabIndex = 2
+        '
+        'cmbOperasi
+        '
+        Me.cmbOperasi.FormattingEnabled = True
+        Me.cmbOperasi.Items.AddRange(New Object() {"+", "-", "*", "/"})
+        Me.cmbOperasi.Location = New System.Drawing.Point(326, 51)
+        Me.cmbOperasi.Name = "cmbOperasi"
+        Me.cmbOperasi.Size = New System.Drawing.Size(121, 28)
+        Me.cmbOperasi.TabIndex = 3
+        '
+        'txtAngka2
+        '
+        Me.txtAngka2.Location = New System.Drawing.Point(453, 53)
+        Me.txtAngka2.Name = "txtAngka2"
+        Me.txtAngka2.Size = New System.Drawing.Size(179, 26)
+        Me.txtAngka2.TabIndex = 4
+        '
+        'btnHitung
+        '
+        Me.btnHitung.Location = New System.Drawing.Point(326, 103)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(121, 43)
+        Me.btnHitung.TabIndex = 5
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = True
+        '
+        'btnBacaHistory
+        '
+        Me.btnBacaHistory.Location = New System.Drawing.Point(326, 217)
+        Me.btnBacaHistory.Name = "btnBacaHistory"
+        Me.btnBacaHistory.Size = New System.Drawing.Size(121, 43)
+        Me.btnBacaHistory.TabIndex = 7
+        Me.btnBacaHistory.Text = "Baca History"
+        Me.btnBacaHistory.UseVisualStyleBackColor = True
+        '
+        'lblHasil
+        '
+        Me.lblHasil.AutoSize = True
+        Me.lblHasil.Location = New System.Drawing.Point(647, 59)
+        Me.lblHasil.Name = "lblHasil"
+        Me.lblHasil.Size = New System.Drawing.Size(52, 20)
+        Me.lblHasil.TabIndex = 9
+        Me.lblHasil.Text = "Hasil :"
+        '
+        'txtHistory
+        '
+        Me.txtHistory.Location = New System.Drawing.Point(141, 152)
+        Me.txtHistory.Multiline = True
+        Me.txtHistory.Name = "txtHistory"
+        Me.txtHistory.ReadOnly = True
+        Me.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtHistory.Size = New System.Drawing.Size(491, 59)
+        Me.txtHistory.TabIndex = 10
+        '
+        'ContextMenuStrip3
+        '
+        Me.ContextMenuStrip3.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ContextMenuStrip3.Name = "ContextMenuStrip3"
+        Me.ContextMenuStrip3.Size = New System.Drawing.Size(61, 4)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtHistory)
+        Me.Controls.Add(Me.lblHasil)
+        Me.Controls.Add(Me.btnBacaHistory)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtAngka2)
+        Me.Controls.Add(Me.cmbOperasi)
+        Me.Controls.Add(Me.txtAngka1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ContextMenuStrip2 As ContextMenuStrip
+    Friend WithEvents txtAngka1 As TextBox
+    Friend WithEvents cmbOperasi As ComboBox
+    Friend WithEvents txtAngka2 As TextBox
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents btnBacaHistory As Button
+
+    Private Sub lblHasil_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Friend WithEvents lblHasil As Label
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Friend WithEvents txtHistory As TextBox
+    Friend WithEvents ContextMenuStrip3 As ContextMenuStrip
+End Class
---
+++ UAS_PEMROGRAMAN_VISUAL/UAS_PEMROGRAMAN_VISUAL/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO
+
+Public Class Form1
+    ' Simpan file di direktori aplikasi agar lebih mudah ditemukan
+    Private filePath As String = Path.Combine(Application.StartupPath, "history.txt")
+
+    ' Prosedur untuk menyimpan riwayat perhitungan ke file
+    Private Sub SimpanHistory(hasil As String)
+        Try
+            Using writer As StreamWriter = File.AppendText(filePath)
+                writer.WriteLine(hasil)
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Gagal menyimpan history: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Prosedur untuk membaca riwayat perhitungan dari file dan menampilkannya di TextBox
+    Private Sub BacaHistory()
+        Try
+            txtHistory.Clear() ' Bersihkan TextBox sebelum menampilkan history
+            If File.Exists(filePath) Then
+                Dim historyText As String = File.ReadAllText(filePath)
+                If historyText.Trim() = "" Then
+                    MessageBox.Show("Riwayat masih kosong.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    txtHistory.Text = historyText
+                End If
+            Else
+                MessageBox.Show("File history belum ada. Lakukan perhitungan dulu!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Gagal membaca history: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Tombol untuk membaca history dari file
+    Private Sub btnBacaHistory_Click(sender As Object, e As EventArgs) Handles btnBacaHistory.Click
+        BacaHistory()
+    End Sub
+
+    ' Tombol untuk menghitung
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim angka1, angka2, hasil As Double
+        Dim operasi As String = cmbOperasi.Text
+
+        If Double.TryParse(txtAngka1.Text, angka1) AndAlso Double.TryParse(txtAngka2.Text, angka2) Then
+            Select Case operasi
+                Case "+"
+                    hasil = angka1 + angka2
+                Case "-"
+                    hasil = angka1 - angka2
+                Case "*"
+                    hasil = angka1 * angka2
+                Case "/"
+                    If angka2 <> 0 Then
+                        hasil = angka1 / angka2
+                    Else
+                        MessageBox.Show("Tidak bisa dibagi dengan nol!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        Exit Sub
+                    End If
+                Case Else
+                    MessageBox.Show("Pilih operasi yang valid!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    Exit Sub
+            End Select
+
+            ' Menampilkan hasil di Label
+            lblHasil.Text = "Hasil: " & hasil.ToString()
+
+            ' Simpan history
+            Dim hasilString As String = angka1 & " " & operasi & " " & angka2 & " = " & hasil
+            SimpanHistory(hasilString)
+        Else
+            MessageBox.Show("Masukkan angka yang valid!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+End Class
